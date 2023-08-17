425cd520a05030214d6f21e1d767875a89e10aea
irma660 <1325455@senati.pe>
2023-08-17 15:45:32 -0500

    elegir opción

--- src/calcu/Calcu.java
+++ src/calcu/Calcu.java
@@ -2,11 +2,16 @@
 package calcu;
 import java.util.Scanner;
 public class Calcu {
+    
 
     
     public static void main(String[] args) {
         System.out.println("CALCULADORA");
-        
+        System.out.println("ELIGE UNA OPCIÓN");
+        System.out.println("1.SUMA");
+        System.out.println("2.RESTA");
+        System.out.println("INGRESA OPCION - >");
+        operacion = teclado.nextInt();
        
     }
     
