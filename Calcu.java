3b9d7e625c3f474456f2edf099a57ba2fd199ebc
JeffBigg <soriaarandajeferson@gmail.com>
2023-08-17 15:32:38 -0500

    ingresar datos

--- Calcu.java
+++ Calcu.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package calcu;
 
-/**
- *
- * @author Alumno
- */
 public class Calcu {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola esto esta empesando");
+        System.out.println("un dato mas");
     }
     
 }
