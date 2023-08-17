7e0ef2d9b07237083429aab72c669a0cc63463c9
irma660 <1325455@senati.pe>
2023-08-17 15:51:32 -0500

    agregando while

--- src/calcu/Calcu.java
+++ src/calcu/Calcu.java
@@ -6,23 +6,25 @@ public class Calcu {
 
     
     public static void main(String[] args) {
-        System.out.println("CALCULADORA");
-        System.out.println("ELIGE UNA OPCIÓN");
-        System.out.println("1.SUMA");
-        System.out.println("2.RESTA");
-        System.out.println("INGRESA OPCION - >");
-        operacion = teclado.nextInt();
-        
-    }else if (operacion == 2){
-            System.out.println("Introduzca el primer número: ");
-            n1 = teclado.nextInt();
-            System.out.println("Introduzca el segundo número: ");
-            n2 = teclado.nextInt();
-            
-            resta = n1 - n2;
-            System.out.println("LA RESTA ES: "+resta);
-            break;
-       
-    }
     
+        while(true){
+            System.out.println("CALCULADORA");
+            System.out.println("ELIGE UNA OPCIÓN");
+            System.out.println("1.SUMA");
+            System.out.println("2.RESTA");
+            System.out.println("INGRESA OPCION - >");
+            operacion = teclado.nextInt();
+
+        }else if (operacion == 2){
+                System.out.println("Introduzca el primer número: ");
+                n1 = teclado.nextInt();
+                System.out.println("Introduzca el segundo número: ");
+                n2 = teclado.nextInt();
+
+                resta = n1 - n2;
+                System.out.println("LA RESTA ES: "+resta);
+                break;
+
+        }
+    }
 }
