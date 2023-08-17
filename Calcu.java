9b925527bea1bfa659b529071cd7d8aec0ed2f8f
JeffBigg <soriaarandajeferson@gmail.com>
2023-08-17 15:45:57 -0500

    agrego la suma

--- Calcu.java
+++ Calcu.java
@@ -7,8 +7,17 @@ public class Calcu {
 
     public static void main(String[] args) {
         //declarando variables
-        int n1,n2, suma, resta,operacion;
+        int n1,n2, suma, resta,operacion = 0;
         Scanner teclado = new Scanner(System.in);
+        
+        if (operacion == 1){
+                System.out.print("Introdusca el primer numero: ");
+                n1 = teclado.nextInt();
+                System.out.print("Introdusca el segundo numero: ");
+                n2 = teclado.nextInt();
+
+                suma = n1 + n2;
+                System.out.println("la suma es "+ suma);
     }
     
 }
