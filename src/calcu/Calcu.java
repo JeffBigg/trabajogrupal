0e234d421f0901395dfdc2d166f8e3beca0c6709
irma660 <1325455@senati.pe>
2023-08-17 15:49:02 -0500

    resta

--- src/calcu/Calcu.java
+++ src/calcu/Calcu.java
@@ -12,6 +12,16 @@ public class Calcu {
         System.out.println("2.RESTA");
         System.out.println("INGRESA OPCION - >");
         operacion = teclado.nextInt();
+        
+    }else if (operacion == 2){
+            System.out.println("Introduzca el primer número: ");
+            n1 = teclado.nextInt();
+            System.out.println("Introduzca el segundo número: ");
+            n2 = teclado.nextInt();
+            
+            resta = n1 - n2;
+            System.out.println("LA RESTA ES: "+resta);
+            break;
        
     }
     
